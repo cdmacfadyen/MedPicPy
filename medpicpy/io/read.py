028a245ed64d90b78d4415792a437738a7e12585
Craig Macfadyen <cdmacfadyen@gmail.com>
2020-08-03 10:18:07 +0100

    load_image now tries to use opencv, then falls back on sitk

--- medpicpy/io/read.py
+++ medpicpy/io/read.py
@@ -4,22 +4,30 @@ import os
 import ntpath
 import numpy as np
 
-
+# TODO: Read more about SITK formats, probably 
+# want it to default to reading with SITK
+# then catch an exception then try with opencv.
 # one stop shop for image reading
 def load_image(path):
     image_name = ntpath.basename(path)
     image_as_array = None
+
     if os.path.isdir(path): # if its a directory its a dicom series or something similar
         series = load_series(path)
         return series
     
     extension = image_name.split(".")[1]
-    if extension == "pgm":
-        image_as_array = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    elif extension == "dcm":    # for loading an individual (2d) dicom
+    if extension == "dcm":    # for loading an individual (2d) dicom
         image = sitk.ReadImage(path)
         image_as_array = sitk.GetArrayFromImage(image)
-        image_as_array = image_as_array[0]  # only the first one since this is a 2d dicom
+        image_as_array = image_as_array[0]  # only the first one since this is a 2d dico
+    else:
+        image_as_array = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        
+        if image_as_array is None:  # opencv couldn't read it
+            image = sitk.ReadImage(path)
+            image_as_array = sitk.GetArrayFromImage(image)
+    
     return image_as_array
 
 
