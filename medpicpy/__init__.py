99331f22fbd198f76ed44189ef86ca7522c5344a
Craig Macfadyen <cdmacfadyen@gmail.com>
2020-08-09 13:38:01 +0100

    Changed init to make importing better and added more general image loading function

---
+++ medpicpy/__init__.py
@@ -0,0 +1,8 @@
+from .parsing_2d import *
+
+from .parsing_3d import get_all_slices_from_series
+from .parsing_3d import get_slices_from_scans
+from .parsing_3d import get_paths_from_ids
+
+from .io import load_image
+from .io import load_series
--- medpicpy/parsing_2d.py
+++ medpicpy/parsing_2d.py
@@ -11,6 +11,7 @@ from . import io
 
 # opt args to add
 #   - resize keeps aspect ratio?
+#TODO: probably change it from taking a dataframe to taking an array (i.e. pd.Series)
 def read_images_from_csv(dataframe, image_name_column, image_dir_path, output_shape):
     """Read in an array of images from paths specified in a csv
 
@@ -39,6 +40,8 @@ def read_images_from_csv(dataframe, image_name_column, image_dir_path, output_sh
 # other encoding is categorical with labelencoder, or none and it just returns the series
 # TODO: probably leave the encoding out and that way they can do whatever they want. 
 # means that this doesn't have to require sklearn. 
+# TODO: kind of useless since they already have the classes as an array,
+# probably just remove
 def read_classes_from_csv(dataframe, classes_column, encoding='one_hot'):
     """Read classes from column in dataframe and optionally 
     transform to one hot or categorical values. 
@@ -68,11 +71,14 @@ def read_classes_from_csv(dataframe, classes_column, encoding='one_hot'):
     print("{} Classes found: {}".format(len(encoder.classes_),encoder.classes_))
     
     return classes
-    
+
+#TODO kind of useless since they already have the bounding boxes as arrays
 def read_bounding_boxes_from_csv(
     dataframe, 
-    centre_x_column, centre_y_column, 
-    width_column, height_column, 
+    centre_x_column, 
+    centre_y_column, 
+    width_column, 
+    height_column, 
     x_scale_factor=1,
     y_scale_factor=1
     ): # for bounding boxes need to know if measurements are in pixels or mm
@@ -150,3 +156,31 @@ def read_classes_in_directory_name(directory, image_file_wildcard, output_shape,
         array[index] = result
         
     return classes, array
+
+
+
+def load_images_from_paths(paths, output_shape):
+    """General image loading function that takes an array of 
+    paths and an output shape and returns the images in 
+    the same order as the paths. Requires every 
+    path to have an image and every image to be resizeable 
+    to the given output shape
+
+    Args:
+        paths (list or array-like): paths of images to load
+        output_shape (tuple): desired shape of each image
+
+    Returns:
+        np.array: all images in numpy format with given shape
+    """
+    array_length = len(paths)
+    array_shape = (array_length,) + output_shape # concat tuples to get shape
+    image_array = np.zeros(array_shape)
+
+    for i in range(0, array_length):
+        image_name = paths[i]
+        image = io.load_image(image_name)
+        resized = cv2.resize(image, output_shape)
+        image_array[i] = resized
+    
+    return image_array
