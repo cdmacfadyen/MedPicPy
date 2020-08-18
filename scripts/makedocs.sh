0b5cd95ff8a4223d9123305cfa7e68de8d3a7366
Craig Macfadyen <cdmacfadyen@gmail.com>
2020-08-18 16:15:22 +0100

    added search widget to docs

--- scripts/makedocs.sh
+++ scripts/makedocs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pdoc medpicpy --html -o temp-docs-dir
+pdoc medpicpy --html -o temp-docs-dir --template-dir ./scripts/pdoc-templates
 mv -f temp-docs-dir/medpicpy/* docs/
 rm -rf temp-docs-dir
