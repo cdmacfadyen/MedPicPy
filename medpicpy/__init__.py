46034d673275d90db075fb8f4822925cac7913d5
Craig Macfadyen <cdmacfadyen@gmail.com>
2020-08-20 10:25:22 +0100

    Added link to wiki in docs page

--- medpicpy/__init__.py
+++ medpicpy/__init__.py
@@ -1,6 +1,10 @@
 """
 MedPicPy provides a simple API for loading medical 
 imaging datasets for use in deep learning projects.
+
+The documentation for the functions shows example usages,
+or see [the wiki](https://github.com/cdmacfadyen/MedPicPy/wiki)
+for examples of how the library is used on real datasets. 
 """
 
 from .parsing import *
