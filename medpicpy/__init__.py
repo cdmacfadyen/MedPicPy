a023114de7a31320704563fe848abe5b4e6f5c3d
Craig Macfadyen <cdmacfadyen@gmail.com>
2020-08-28 15:26:35 +0100

    Add cache dir for mmap to use

--- medpicpy/__init__.py
+++ medpicpy/__init__.py
@@ -6,7 +6,17 @@ The documentation for the functions shows example usages,
 or see [the wiki](https://github.com/cdmacfadyen/MedPicPy/wiki)
 for examples of how the library is used on real datasets. 
 """
+import os
+import shutil
 
 from .parsing import *
 from .io import *
 from .paths import *
+
+cache_dir = "medpicpy_cache/"
+
+if not os.path.exists(cache_dir):
+    os.mkdir(cache_dir)
+else:
+    for cache_file in os.listdir(cache_dir):
+        os.remove(cache_dir + cache_file)
