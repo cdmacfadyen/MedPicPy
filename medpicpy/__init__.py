1b8ae9d778fa235a4701efb9a0cf1de26e3cdae1
Craig Macfadyen <cdmacfadyen@gmail.com>
2020-08-29 18:26:12 +0100

    Add link to github page to docs

--- medpicpy/__init__.py
+++ medpicpy/__init__.py
@@ -3,8 +3,9 @@ MedPicPy provides a simple API for loading medical
 imaging datasets for use in deep learning projects.
 
 The documentation for the functions shows example usages,
-or see [the wiki](https://github.com/cdmacfadyen/MedPicPy/wiki)
-for examples of how the library is used on real datasets. 
+see [the wiki](https://github.com/cdmacfadyen/MedPicPy/wiki)
+for examples of how the library is used on real datasets 
+or check out the [GitHub page](https://github.com/cdmacfadyen/MedPicPy) to install/contribute.
 """
 import os
 import shutil
