d8e5017889742118e49bd80c73529350a3e6eb31
Craig <cdmacfdayen@gmail.com>
2021-01-27 10:17:08 +0000

    Documented config file

--- medpicpy/config.py
+++ medpicpy/config.py
@@ -1,3 +1,19 @@
+"""
+This module allows setting of some global values that 
+medpicpy references internally. 
+
+cache_location refers to where the image data is stored 
+when `use_memory_mapping` is set in a function call.
+
+suppress_errors means that exceptions will not be thrown if 
+an invalid path is read as an image. This may be desired for some 
+large projects where there are non-image files hidden in the 
+dataset. 
+
+rescale means that the images are automatically scaled 
+between 0 and 1 as they are loaded. Useful for machine learning 
+projects. 
+"""
 import os
 
 cache_location = "."
